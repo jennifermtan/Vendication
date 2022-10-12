2d5588fc98ba436a6a423603a2782d4af570b344
Marian Derias <marianderias@gmail.com>
2022-10-12 17:21:31 +1100

    added planned classes for sprint 1

---
+++ SOFT2412-A2/app/src/main/java/SOFT2412/A2/VendingMachine.java
@@ -0,0 +1,9 @@
+package SOFT2412.A2;
+import java.util.*;
+
+public class VendingMachine {
+    private Map<String, Integer> cash = new HashMap<String, Integer>();
+    private List<Customer> customers = new ArrayList<>();
+    private List<Card> cards = new ArrayList<Card>();
+
+}
