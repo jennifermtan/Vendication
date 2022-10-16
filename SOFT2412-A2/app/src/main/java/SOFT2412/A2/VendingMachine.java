0aa460007e016f134e617947f1c261f2fbf7cdf5
unknown <marianderias@gmail.com>
2022-10-16 20:15:00 +1100

    bolstered user interface methods to buy an item with cash and simplified vendingMachine payWithCash method. Also allowed user to exit cash transaction

--- SOFT2412-A2/app/src/main/java/SOFT2412/A2/VendingMachine.java
+++ SOFT2412-A2/app/src/main/java/SOFT2412/A2/VendingMachine.java
@@ -11,32 +11,40 @@ public class VendingMachine {
     private List<Card> cards = new ArrayList<Card>();
 
     // A hashmap of the form: foodType: quantity in the vending machine
-    private Map<Food, Integer> inventory = new HashMap<>();
+    private HashMap<Food, Integer> inventory = new HashMap<Food, Integer>();
 
     // A hashmap that records all the cash in the form cashType: quantity
-    private HashMap<String, Integer> cash = new HashMap<String, Integer>();
+    private HashMap<String, Integer> cash = new LinkedHashMap<String, Integer>();
 
     public VendingMachine(){
-        // Load in the cash and the inventory from "inventory.txt" and "cash.txt" files
+        // Load in the cash and the inventory from "inventory.txt" and "cash.txt" files using methods in case we need to reload them
+        // load in the cash numbers from "cash.txt"
+        loadCash();
+
+        // load in the inventory from "inventory.txt"
+        loadInventory();
+    }
+    public void loadCash(){
         try{
-            // load in the cash numbers from "cash.txt"
             File cashFile = new File("./src/main/resources/cash.txt");
             Scanner scan1 = new Scanner(cashFile);
             while (scan1.hasNextLine()){
                 String[] line = scan1.nextLine().split(", ");
                 cash.put(line[0], Integer.valueOf(line[1]));
             }
-
-            // load in the inventory from "inventory.txt"
+        }
+        catch(FileNotFoundException fe){}
+    }
+    public void loadInventory(){
+        try{
             File invenFile = new File("./src/main/resources/inventory.txt");
             Scanner scan2 = new Scanner(invenFile);
             while (scan2.hasNextLine()){
                 String[] line = scan2.nextLine().split(", ");
                 inventory.put(new Food(line[0], line[1], line[2], Double.parseDouble(line[3])), Integer.valueOf(line[4]));
             }
-
         }
-        catch(FileNotFoundException f){System.out.println(f);}
+        catch(FileNotFoundException fe){}
     }
 
     // I'm following Frank's structure that user input will be of the form:
@@ -44,70 +52,52 @@ public class VendingMachine {
     // i.e. userType paymentType quantity itemCode givenMoney
     // GivenMoney can have a variable length so it's simply all the inputs after the itemCode
     public String payByCash(int quantity, String itemCode, String givenMoney){
-        Food toBuy = searchByItemCode(itemCode);
-        double toPay = toBuy.getCost() * quantity;
+
+        double toPay = calculateToPay(itemCode, quantity);
         String[] givenCash = givenMoney.split(" ");
-        double paid = 0;
-
-        // Go through every cash item and calculate how much money has been given
-        for (String typeOfCash: givenCash){
-            try {
-                String[] thisCash = typeOfCash.split("\\*");
-                if (thisCash[0].equals("")){
-                    return "not enough paid";
-                }
-                if (!cash.keySet().contains(thisCash[0])) {
-                    return "incorrect format";
-                }
-                // Check if the input type given is a dollar (starts with $)
-                if (Character.toString(thisCash[0].charAt(0)).equals("$")) {
-                    paid += (Double.parseDouble(thisCash[0].substring(1)) * Double.parseDouble(thisCash[1]));
-                }
-                // Otherwise it's cents
-                else {
-                    paid += ((Double.parseDouble(thisCash[0].substring(0, thisCash[0].length() - 1)) / 100) * Double.parseDouble(thisCash[1]));
-                }
-
-            }
-            catch(NumberFormatException f){ return "incorrect format";}
-
-        }
+        double paid = calculateGivenCash(givenCash);
 
         // Check that they've given enough
         if (toPay > paid && Math.abs(toPay - paid) >= 0.00001){
-            return "not enough paid";
+            throw new ArithmeticException();
         }
 
         DecimalFormat df = new DecimalFormat("0.00");
         BigDecimal change = new BigDecimal(paid - toPay);
 
-        // Calculate change breakdown
+        // Calculate change
+        Map<String, Integer> changeCash = calculateChange(change);
+
+        // Process final result summary
+        String changeBreakdown = "";
+        for (Map.Entry<String, Integer> payment: changeCash.entrySet()){
+            changeBreakdown += (" (" + payment.getKey() + "*" + payment.getValue() + ")");
+        }
+
+        String resultString = "Transaction successful!\n" + "Paid: $" + df.format(paid) + "\nDue: $" + df.format(toPay) + "\nChange: $" + df.format(change);
+        if (changeBreakdown != ""){
+            resultString += ("\n\nChange Breakdown: \n" + changeBreakdown);
+        }
+        return resultString;
+
+
+    }
+
+    public Map<String, Integer> calculateChange(BigDecimal change){
         Map<String, Integer> changeCash = new LinkedHashMap<String, Integer>();
-        List<String> cashTypes = new ArrayList<>(){{
-           add("$100");
-           add("$50");
-           add("$20");
-           add("$10");
-           add("$5");
-           add("$2");
-           add("$1");
-           add("50c");
-           add("20c");
-           add("10c");
-           add("5c");
-        }};
-        BigDecimal changeNum = change;
         int prevChange = -1;
         int currChange = 0;
         // This is a disgusting line but basically it's giving change to the customer while there is still change to be given (change > 0)
         while (change.subtract(new BigDecimal(0.0001)).compareTo(new BigDecimal(0)) == 1){
             // If we haven't added any new change this round, then we can't figure out a way to give them change so return
             if (prevChange == currChange){
-                return "no possible change";
+                // if there was no possible change then we should return all the cash to the hashmap by reading in from cash.txt
+                loadCash();
+                throw new IllegalStateException();
             }
             prevChange = currChange;
             // Check every cash value to try to add a coin or note to the change
-            for (String cashType: cashTypes){
+            for (String cashType: cash.keySet()){
                 BigDecimal value;
 
                 // Check how much this cash type is worth according to whether it's a dollar or cent
@@ -130,18 +120,41 @@ public class VendingMachine {
                     else{
                         changeCash.put(cashType, changeCash.get(cashType) + 1);
                     }
+                    cash.put(cashType, cash.get(cashType) - 1);
 
                 }
             }
-
         }
-        String changeBreakdown = "";
-        for (Map.Entry<String, Integer> payment: changeCash.entrySet()){
-            changeBreakdown += (" (" + payment.getKey() + "*" + payment.getValue() + ")");
-        }
-        return "Transaction successful!\n" + "Paid: $" + df.format(paid) + "\nDue: $" + df.format(toPay) + "\nChange: $" + df.format(changeNum)
-                + "\n\nChange Breakdown: \n" + changeBreakdown;
 
+        return changeCash;
+    }
+
+    public double calculateToPay(String itemCode, int quantity){
+        Food toBuy = searchByItemCode(itemCode);
+        return toBuy.getCost() * quantity;
+    }
+
+    public double calculateGivenCash(String[] givenCash){
+        double paid = 0;
+        // Go through every cash item and calculate how much money has been given
+        for (String typeOfCash: givenCash){
+            String[] thisCash = typeOfCash.split("\\*");
+            if (thisCash[0].equals("")){
+                throw new ArithmeticException();
+            }
+
+            // Add the given cash into our list of cash
+            cash.put(thisCash[0], cash.get(thisCash[0]) + Integer.parseInt(thisCash[1]));
+            // Check if the input type given is a dollar (starts with $)
+            if (Character.toString(thisCash[0].charAt(0)).equals("$")) {
+                paid += (Double.parseDouble(thisCash[0].substring(1)) * Double.parseDouble(thisCash[1]));
+            }
+            // Otherwise it's cents
+            else {
+                paid += ((Double.parseDouble(thisCash[0].substring(0, thisCash[0].length() - 1)) / 100) * Double.parseDouble(thisCash[1]));
+            }
+        }
+        return paid;
     }
 
     public Food searchByItemCode(String itemCode){
@@ -153,6 +166,7 @@ public class VendingMachine {
         return null;
     }
 
+    // This method rounds a BigDecimal to the given number of d.p
     public static BigDecimal round(BigDecimal value, BigDecimal increment, RoundingMode roundingMode) {
         if (increment.signum() == 0) {
             // 0 increment does not make much sense, but prevent division by 0
@@ -163,5 +177,9 @@ public class VendingMachine {
             return result;
         }
     }
+
+    public HashMap<String, Integer> getCash(){return cash;}
+    public HashMap<Food, Integer> getInventory(){return inventory;}
+    public List<Customer> getCustomers(){return customers;}
 
 }
