d5a949fb0eb3dc18ff1fdeef4ebda929c6aa1002
pbha6036 <pbha6036@uni.sydney.edu.au>
2022-10-17 15:39:58 +1100

    Work in progress

--- SOFT2412-A2/app/src/main/java/SOFT2412/A2/Card.java
+++ SOFT2412-A2/app/src/main/java/SOFT2412/A2/Card.java
@@ -2,5 +2,19 @@ package SOFT2412.A2;
 import java.util.*;
 
 public class Card extends Payment{
-    public List<Card> cards = new ArrayList<Card>();
+    private name;
+    private number;
+    public static List<Card> cards = new ArrayList<Card>();
+    public Card(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
 }
--- SOFT2412-A2/app/src/main/java/SOFT2412/A2/User.java
+++ SOFT2412-A2/app/src/main/java/SOFT2412/A2/User.java
@@ -1,37 +1,1 @@
-package SOFT2412.A2;
-import java.util.*;
-import java.io.*;
-
-public abstract class User {
-    private static Map<String, String> userLogins = new HashMap<String, String>();
-    private static List<User> users = new ArrayList<User>();
-    protected String name;
-    protected String username;
-    protected String password;
-    protected Card card;
-
-    public User(String name, String username, String password) {
-        this.name = name;
-        this.username = username;
-        this.password = password;
-    }
-
-    public static void loadUsers() {
-        String[] userInfo;
-        try {
-            Scanner usersFile = new Scanner(new File("./src/main/resources/users.txt"));
-            while(usersFile.hasNextLine()) {
-                String line = usersFile.nextLine();
-                userInfo = line.split(", ");
-                if(!userLogins.containsKey(userInfo[1]))
-                    userLogins.put(userInfo[1], userInfo[2]);
-                else
-                    System.out.println("That username already exists.");
-            }
-            usersFile.close();
-        }
-        catch(FileNotFoundException e) {
-            System.out.println("loadUsers: File not found exception.");
-        }
-    }
-}
+package SOFT2412.A2;import java.util.*;import java.io.*;public abstract class User {    private static Map<String, String> userLogins = new HashMap<String, String>();    private static List<User> users = new ArrayList<User>();    protected String name;    protected String username;    protected String password;    protected Card card;    public User(String name, String username, String password) {        this.name = name;        this.username = username;        this.password = password;    }    // Save a card for the user    public void saveCard(Card card) {        this.card = card;        JSONObject json = loadJSONArray("./src/main/resources/creditCards.json");    }    public static void loadUsers() {        String[] userInfo;        try {            Scanner usersFile = new Scanner(new File("./src/main/resources/users.txt"));            while(usersFile.hasNextLine()) {                String line = usersFile.nextLine();                userInfo = line.split(", ");                if(!userLogins.containsKey(userInfo[1]))                    userLogins.put(userInfo[1], userInfo[2]);                else                    System.out.println("That username already exists.");            }            usersFile.close();        }        catch(FileNotFoundException e) {            System.out.println("loadUsers: File not found exception.");        }    }}
