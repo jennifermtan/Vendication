70dbac899c96b2a240726ae02470e3bd93e55645
Marian Derias <marianderias@gmail.com>
2022-10-28 11:55:22 +1100

    added summaries for transactions to their respective user classes

--- SOFT2412-A2/app/src/main/java/SOFT2412/A2/Cashier.java
+++ SOFT2412-A2/app/src/main/java/SOFT2412/A2/Cashier.java
@@ -1,7 +1,32 @@
 package SOFT2412.A2;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 public class Cashier extends User{
     public Cashier(String name, String username, String password) {
         super(name, username, password);
+    }
+
+    public String getTransactionSummary(){
+        String allTransactions="";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        allTransactions += "--------------------------------------------------------------------\n";
+        allTransactions += "|  Snack Name  |  Paid  | Change | Payment Method |       Time      \n";
+        allTransactions += "--------------------------------------------------------------------\n";
+        for (List<Transaction> tList: Transaction.userTransactions.values()){
+            for (Transaction t: tList) {
+
+                allTransactions += ("|" + UserInterface.foodDetailString(14, t.getItemSold().getName()) +  UserInterface.foodDetailString(8, "$" + String.valueOf(t.getPaid())) + UserInterface.foodDetailString(8, "$" + String.valueOf(t.getChange()))
+                        + UserInterface.foodDetailString(16, t.getPaymentMethod()) + UserInterface.foodDetailString(16, t.getTimeSold().format(formatter)) + "\n");
+            }
+        }
+
+        for (Transaction t: Transaction.anonTransactions){
+            allTransactions += ("|" + UserInterface.foodDetailString(14, t.getItemSold().getName()) +  UserInterface.foodDetailString(8, "$" + String.valueOf(t.getPaid())) + UserInterface.foodDetailString(8, "$" + String.valueOf(t.getChange()))
+                    + UserInterface.foodDetailString(16, t.getPaymentMethod()) + UserInterface.foodDetailString(16, t.getTimeSold().format(formatter)) + "\n");
+        }
+        allTransactions += "--------------------------------------------------------------------";
+        return allTransactions;
     }
 }
