9ac391d65342117a857b9ee7aaeed57e540c52b6
Marian Derias <marianderias@gmail.com>
2022-10-30 20:23:58 +1100

    added method to let Owner add a user

--- SOFT2412-A2/app/src/main/java/SOFT2412/A2/Owner.java
+++ SOFT2412-A2/app/src/main/java/SOFT2412/A2/Owner.java
@@ -1,8 +1,10 @@
 package SOFT2412.A2;
 
+import java.io.*;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class Owner extends User{
     public Owner(String name, String username, String password) {
@@ -29,7 +31,7 @@ public class Owner extends User{
         List<User> allUsers = User.getUsers();
         User toRemove = null;
         for (User u: allUsers){
-            if (u.getUsername().equals(name)){
+            if (u.getUsername().equals(username)){
                 if (u.getClass().equals("cashier") || u.getClass().equals("seller")){
                     toRemove = u;
                 }
@@ -44,8 +46,38 @@ public class Owner extends User{
         // Remove from list of users
         allUsers.remove(toRemove);
 
-        //Remove from users.txt
+        File currUsers = new File("./src/main/resources/users.txt");
+        File newUsers = new File("./src/main/resources/tempUsers.txt");
+        //Remove this user from users.txt by making a new file with all of the users except the user we want to delete
+        try{
+            FileWriter fw = new FileWriter("./src/main/resources/tempUsers.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
 
+            FileReader fr = new FileReader("./src/main/resources/users.txt");
+            BufferedReader br = new BufferedReader(fr);
+
+            String currentLine;
+            while ((currentLine = br.readLine()) != null){
+                // Only write this line from users.txt into tempUsers.txt if it's not the user we want to delete
+                String[] info = currentLine.split(", ");
+                if (!info[2].equals(username)){
+                    pw.println(currentLine);
+                }
+            }
+
+            pw.flush();
+            pw.close();
+            fr.close();
+            br.close();
+            bw.close();
+            fw.close();
+
+            currUsers.delete();
+            File dump = new File("./src/main/resources/users.txt");
+            newUsers.renameTo(dump);
+        }
+        catch(IOException fe){System.out.println(fe);}
     }
 
     // Edit the change and update cash.txt
